6282ea7b64a4b283f66bab4fd9a60126475eefd1
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-08 21:50:11 +0530

    Add files via upload

---
+++ bunny_key.py
@@ -0,0 +1,18 @@
+
+import mysql.connector
+con = mysql.connector.connect(
+            host='127.0.0.1',
+            user='root',
+            passwd='bunny',
+            auth_plugin='mysql_native_password',
+            database='secretkeys'
+            
+)
+cor=con.cursor()
+cor.execute("select * from secret;")
+r=cor.fetchone()
+con.close()
+api_key=r[0]
+access_id= r[1]
+app_pass=r[2]
+app_email=r[3]
